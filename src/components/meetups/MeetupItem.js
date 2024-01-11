c589b6d1d2d3e865e84a80d156def5e5a288b9e8
Susnata Dutta <susnata.dutta@indusnet.co.in>
2024-01-11 16:26:54 +0530

    tested commit

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -36,7 +36,7 @@ function MeetupItem(props) {
         </div>
         <div className={classes.actions}>
           <button onClick={toggleFavoriteStatusHandler}>
-            {itemIsFavorite ? "Remove from Favorites" : "To Favorites"}
+            {itemIsFavorite ? "Remove from Favorites" : "Add To Favorites"}
           </button>
         </div>
       </Card>
